d2109dfbf00da3e525add621207a1e718960d278
Takuto <152110734+Takuto329@users.noreply.github.com>
2024-06-23 16:34:25 +0900

    JDのところがリストに表示されない

--- aperture/transit_restart.py
+++ aperture/transit_restart.py
@@ -4,9 +4,27 @@ from iraf import noao
 from iraf import digiphot
 from iraf import apphot
 from iraf import images
+import pandas as pd 
 import os
+import matplotlib.pyplot as plt
 
-iraf.unlearn('apphot')  # パラメータの初期化
+
+object = input("star_data(e.g.240128):")
+
+#configファイルから各種パラメータをとってくる
+config_path = f"/Users/takuto/iriki/{object}/config.txt"
+config = pd.read_csv(config_path, delimiter='\t')
+data = pd.DataFrame(config)
+
+date = data.loc[data['parameter'] == 'date', 'value'].values[0]
+start_file = int(data.loc[data['parameter'] == 'start_file', 'value'].values[0])
+end_file = int(data.loc[data['parameter'] == 'end_file', 'value'].values[0])
+aperture = data.loc[data['parameter'] == 'aperture_radius', 'value'].values[0]
+annulus = data.loc[data['parameter'] == 'annulus', 'value'].values[0]
+dannulus = data.loc[data['parameter'] == 'dannulus', 'value'].values[0]
+
+#パラメータを決める
+iraf.unlearn('apphot')  
 iraf.apphot.datapars.datamax = 20000
 iraf.apphot.datapars.readnoise = 30
 iraf.apphot.datapars.epadu = 8
@@ -16,9 +34,9 @@ iraf.apphot.daofind.interac = 'no'
 iraf.apphot.daofind.verify = 'no'
 iraf.apphot.datapars.fwhmpsf = 3
 iraf.apphot.centerpars.cbox = 5
-iraf.apphot.fitskypars.annulus = 5
-iraf.apphot.fitskypars.dannulus = 10
-iraf.apphot.photpars.apertures = 5
+iraf.apphot.fitskypars.annulus = annulus
+iraf.apphot.fitskypars.dannulus = dannulus
+iraf.apphot.photpars.apertures = aperture
 iraf.apphot.phot.interactive = 'no'
 iraf.apphot.phot.verify = 'no'
 iraf.apphot.phot.verbose = 'no'
@@ -33,13 +51,49 @@ iraf.imstat.fields = 'sum,area'
 iraf.imstat.format = 'yes'
 iraf.imstat.cache = 'yes'
 
-image_path = "/Users/takuto/iriki/data/GJ3470"
-myimage = "g240128-0103.fits"
-full_image_path = os.path.join(image_path, myimage)
 
-a = "/Users/takuto/iriki/data/GJ3470/a.txt"
+#パスの指定
+image_path = f"/Users/takuto/iriki/{object}/data"
+coo_path = f"/Users/takuto/iriki/{object}/1.coo"
+output_path = f"/Users/takuto/iriki/{object}/value.txt"
+output_path2 = f"/Users/takuto/iriki/{object}"
 
-iraf.phot(full_image_path, coords=a, output='/Users/takuto/iriki/test/4.txt')
+
+#解析
+FLUX = []
+TIME = []
+for files in range(start_file,end_file+1):
+
+    image = f"{image_path}/g{date}-{files:04d}.fits"
+
+    if not os.path.exists(image):
+        print("File {} does not exist. Skipping.".format(image))
+        continue
+
+
+    iraf.phot(image, coords=coo_path, output=output_path) #測光
+    flux = iraf.pdump(output_path, fields="FLUX",expr="yes",Stdout=1) #qdumpでFluxをとってくる
+    FLUX.append(flux)
+
+    JD_value = iraf.imgets(image,"UT",Stdout=1)
+    print(JD_value)
+
+
+
+
+
+
+    
+    
+
+
+
+
+    
+
+
+
+    
     
 
 
