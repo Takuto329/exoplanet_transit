e4b3cb3c735c5c9e3accac72b201069cd34e7ec4
Takuto <152110734+Takuto329@users.noreply.github.com>
2024-06-24 20:28:32 +0900

    多分完成

--- 1次処理/sky.py
+++ 1次処理/sky.py
@@ -1,75 +1,78 @@
-#!/usr/bin/env python3
-from pyraf import iraf #sky引き専用
+from pyraf import iraf
 import pandas as pd
 import os
 iraf.unlearn('imstat')
 iraf.imstat.field = 'midpt'
 iraf.imstat.nclip = 10
 
-data = pd.read_csv('my_observe_log.csv', delimiter=',')
-# object列をインデックスに設定
-data.set_index('object', inplace=True)
+# object = input("star_data(e.g.240128):")
+object = 'GJ3470_240128'
 
-# 特定のオブジェクトの情報を取得する関数を定義
-def get_info(object_name):
-    if object_name in data.index:
-        row = data.loc[object_name]
-        day = row['yymmdd']
-        start_file = row['start_file']
-        end_file = row['end_file']
-        sky_start= row['sky_start']
-        sky_end = row['sky_end']
+#configファイルから各種パラメータをとってくる
+config_path = f"/Users/takuto/iriki/{object}/config.txt"
+config = pd.read_csv(config_path, delimiter='\t')
+data = pd.DataFrame(config)
 
-        return f"day={day}, start_file={start_file}, end_file={end_file}, sky_start={sky_start}, sky_end={sky_end}"
-    else:
-        return "Object not found"
+date = data.loc[data['parameter'] == 'date', 'value'].values[0]
+start_file = int(data.loc[data['parameter'] == 'start_file', 'value'].values[0])
+end_file = int(data.loc[data['parameter'] == 'end_file', 'value'].values[0])
+sky_start_file = int(data.loc[data['parameter'] == 'sky_start_file', 'value'].values[0])
+sky_end_file = int(data.loc[data['parameter'] == 'sky_end_file', 'value'].values[0])
+all_start_file = int(data.loc[data['parameter'] == 'all_start_file', 'value'].values[0])
+all_end_file = int(data.loc[data['parameter'] == 'all_end_file', 'value'].values[0])
 
-object_name = input("目標天体名を入力してください: ")
-row = data[data.index == object_name]
 
-if not row.empty:
-    day = row['yymmdd'].values[0]
-    start_file = row['start_file'].values[0]
-    end_file = row['end_file'].values[0]
-    sky_start = row['sky_start'].values[0]
-    sky_end = row['sky_end'].values[0]
+#パスの指定
+raw_image_path = f"/Users/takuto/iriki/{object}/raw_image"
+reduction_image_path = f"/Users/takuto/iriki/{object}/reduction_image"
+sky_image_path = f"/Users/takuto/iriki/{object}/sky_image"
+flat_image_path = f"/Users/takuto/iriki/flat_image"
 
-os.chdir(object_name)
-a = []
-band_list = ['j', 'h', 'k']
-for band in band_list:
-    for i in range(int(sky_start), (int(sky_end) + 1) ):
-        origin_image = "{}{}-{:04d}.fits".format(band, day, i)
-        if not os.path.exists(origin_image):
-            print("File {} does not exist. Skipping.".format(origin_image))
+
+
+for band in ['j', 'h', 'k']:
+   
+
+#---------------------------------------------------------------------------------------------------------------
+    #すべての画像をフラットで割る
+    for all_files in range(all_start_file,all_end_file+1):
+
+        raw_image = f"{raw_image_path}/{band}{date}-{all_files:04d}.fits"
+
+        if not os.path.exists(raw_image):
+            print("File {} does not exist. Skipping.".format(raw_image))
             continue
-        cut_image = "sky{}{}-{:04d}.fits".format(band, day, i)
 
-        iraf.imcopy(origin_image + "[10:328,10:265]", cut_image)
-        result = iraf.imstat(image=cut_image, fields='midpt', nclip=10, Stdout=1)
-        midpoint = float(result[1])  # Assuming midpt is the second value in the imstat output
-        a.append(midpoint)
-        iraf.imarith(cut_image, '-', midpoint, cut_image)
+        reduction_image = f"{reduction_image_path}/{band}{date}-{all_files:04d}.fits"
 
+        
+        iraf.imarith(raw_image, '/', f'{flat_image_path}/{band}flat.fits', reduction_image) #ここで割ってる
+#---------------------------------------------------------------------------------------------------------------
+    #skyを作る
+    sky = []
+    for sky_files in range(sky_start_file,sky_end_file+1):
 
+        image = f"{reduction_image_path}/{band}{date}-{sky_files:04d}.fits"
 
-    for i in range(int(start_file), (int(end_file) + 1) ):
-        origin_image = "{}{}-{:04d}.fits".format(band, day, i)
-        if not os.path.exists(origin_image):
-            print("File {} does not exist. Skipping.".format(origin_image))
+        if not os.path.exists(image):
+            print("File {} does not exist. Skipping.".format(image))
             continue
-        cut_image = "observe{}{}-{:04d}.fits".format(band, day, i)
 
-        iraf.imcopy(origin_image + "[10:328,10:265]", cut_image)
+        sky.append(image)
+    sky_input = ",".join(sky)
+    iraf.imcomb(input=sky_input, output=f'{sky_image_path}/SKY_{band}.fits', combine="median", scale="none") 
+#---------------------------------------------------------------------------------------------------------------
+    #skyを引く
+    for raw_files in range(start_file,end_file+1):
+        reduction_image = f"{reduction_image_path}/{band}{date}-{raw_files:04d}.fits"
 
+        if not os.path.exists(reduction_image):
+            print("File {} does not exist. Skipping.".format(reduction_image))
+            continue
 
-
-    # 画像の結合を実行
-    iraf.imcomb(input="sky{}{}-*.fits".format(band, day), output=f'SKY_{object_name}_{"{}".format(band)}.fits', combine="median", scale="none") #dith9をfusion
-
-
-    for i in range(int(start_file), (int(end_file) + 1) ):
-        iraf.imarith("observe{}{}-{:04d}.fits".format(band, day, i), '-', f'SKY_{object_name}_{"{}".format(band)}.fits', "observe{}{}-{:04d}.fits".format(band, day, i)) #測光したい画像からスカイを引く
+        iraf.imarith(reduction_image, '-', f'{sky_image_path}/SKY_{band}.fits', reduction_image) #測光したい画像からスカイを引く
+        
+        
 
 
 
