2c3b7e5066f9170bb0e70be96a0c3cb91c7dde93
Takuto <152110734+Takuto329@users.noreply.github.com>
2024-06-29 19:09:08 +0900

    opencvを使って中心を決定、その後それを.cooファイルに書き込み中心を変えていく

--- aperture/transit_restart.py
+++ aperture/transit_restart.py
@@ -5,14 +5,18 @@ from iraf import digiphot
 from iraf import apphot
 from iraf import images
 from astropy.io import fits
+from astropy.time import Time
 import pandas as pd 
 import os
 import numpy as np
+import datetime
+import cv2
+from scipy.ndimage import center_of_mass
 import matplotlib.pyplot as plt
 
 
-object = input("star_data(e.g.240128):")
-# object = 'GJ3470_240128'
+# object = input("star_data(e.g.240128):")
+object = 'GJ1214'
 
 #configファイルから各種パラメータをとってくる
 config_path = f"/Users/takuto/iriki/{object}/config_file/config.txt"
@@ -66,10 +70,23 @@ object_output_path = f"/Users/takuto/iriki/{object}/data/object_value.txt"
 compa_output_path = f"/Users/takuto/iriki/{object}/data/compa_value.txt"
 
 
-iraf.phot(f"{image_path}/j{date}-{start_file:04d}.fits", coords=object_path, output=object_output_path)
-object_xcenter = iraf.pdump(object_output_path, fields="XCENTER",expr="yes",Stdout=1)
-object_ycenter = iraf.pdump(object_output_path, fields="YCENTER",expr="yes",Stdout=1)
-
+def create_annulus_mask(shape, center, inner_radius, outer_radius):
+    """
+    Create a binary mask with an annulus (a ring).
+    
+    Parameters:
+    shape (tuple): Shape of the output mask (height, width).
+    center (tuple): Center of the annulus (y, x).
+    inner_radius (float): Inner radius of the annulus.
+    outer_radius (float): Outer radius of the annulus.
+    
+    Returns:
+    numpy.ndarray: Binary mask with an annulus.
+    """
+    y, x = np.ogrid[:shape[0], :shape[1]]
+    dist_from_center = np.sqrt((x - center[1])**2 + (y - center[0])**2)
+    mask = (dist_from_center >= inner_radius) & (dist_from_center <= outer_radius)
+    return mask
 
 #解析
 FLUX = []
@@ -81,8 +98,8 @@ AIRMASS = []
 
 for files in range(start_file,end_file+1):
 
-    image = f"{image_path}/j{date}-{files:04d}.fits"
-    # image = f"{image_path}/hf{files:04d}.fits"
+    # image = f"{image_path}/j{date}-{files:04d}.fits"
+    image = f"{image_path}/hf{files:04d}.fits" #GJ1214をするとき用(廃線みたいでいいね)
 
     if not os.path.exists(image):
         print("File {} does not exist. Skipping.".format(image))
@@ -93,33 +110,134 @@ for files in range(start_file,end_file+1):
 
     object_flux_data = iraf.pdump(object_output_path, fields="FLUX",expr="yes",Stdout=1) #pdumpでmagをとってくる
     object_error_data = iraf.pdump(object_output_path, fields="merr",expr="yes",Stdout=1)
-    object_xcenter = iraf.pdump(object_output_path, fields="XCENTER",expr="yes",Stdout=1)
-    object_ycenter = iraf.pdump(object_output_path, fields="YCENTER",expr="yes",Stdout=1)
-
     object_flux = float(object_flux_data[0].strip())
     object_error = 10**(float(object_error_data[0].strip()) / 2.5)
-    objectx = 86.80 - float(object_xcenter[0].strip())
-    objecty = 65.06 - float(object_ycenter[0].strip())
-
     
     iraf.phot(image, coords=compa_path, output=compa_output_path) #比較星の測光
 
     compa_flux_data = iraf.pdump(compa_output_path, fields="FLUX",expr="yes",Stdout=1) #pdumpでmagをとってくる
-    
     compa_flux = float(compa_flux_data[0].strip())
     
-
     flux = object_flux / compa_flux #fluxを割って相対的にする
     FLUX.append(flux)
     ERROR.append(object_error)
-    X.append(objectx)
-    Y.append(objecty)
+
+   #--------------------------------------------------------------
+    hdu = fits.open(image)
+
+    data = hdu[0].data
+    x_center, y_center = 580, 495
+    radius = 20
+
+    y, x = np.ogrid[:data.shape[0], :data.shape[1]]
+    mask = (x - x_center)**2 + (y - y_center)**2 <= radius**2
+    masked_data = np.where(mask, data, 0)
+    y_centroid, x_centroid = center_of_mass(masked_data)
+
+    x_M = x_center - x_centroid
+    y_M = y_center - y_centroid
+
+    X.append(x_M)
+    Y.append(y_M)
+
+    # TIME.append(hda[0].header["MJD"]) #JDをとってくる
+    AIRMASS.append(hdu[0].header["AIRMASS"])
 
 
-    hda = fits.open(image)
-    TIME.append(hda[0].header["MJD"]) #JDをとってくる
-    AIRMASS.append(hda[0].header["AIRMASS"])
-    hda.close()
+
+    DATE_UTC = hdu[0].header["DATE_UTC"]
+    TIME_UTC = hdu[0].header["TIME_UTC"]
+
+    # UTC日時を指定
+    datetime_utc = DATE_UTC + ' ' + TIME_UTC
+    
+    # astropy.timeを使ってUTC日時をMJDに変換
+    time = Time(datetime_utc, format='iso', scale='utc')
+    mjd = time.mjd
+    
+    # TIME列にMJDを追加
+    TIME.append(mjd)
+
+ #--------------------------------------------------------------
+    sky_center = (580, 495)  # (y, x)
+    inner_radius = 24  # 内半径
+    outer_radius = 29  # 外半径
+
+    # 中抜き円状のsky領域を作成し、その平均カウント数を計算
+    mask = create_annulus_mask(data.shape, sky_center, inner_radius, outer_radius)
+    sky_region = data[mask]
+    sky_threshold = np.max(sky_region)
+
+    
+    x_start, x_end = 490, 670
+    y_start, y_end = 410, 590
+
+    region = data[y_start:y_end, x_start:x_end]
+
+    binary_region = (region > sky_threshold).astype(int)
+
+    # OpenCVを使用して星を検出し、その中心を求める
+    binary_image = (binary_region * 255).astype(np.uint8)  # OpenCVが扱いやすいように変換
+
+    contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+
+    # 星の中心を求める
+
+    
+    for contour in contours:
+        if cv2.contourArea(contour) < 10:
+            continue
+        (x, y), radius = cv2.minEnclosingCircle(contour)
+        xz = x + 490
+        yz = y + 410
+        with open(object_path, mode='w') as f:
+            f.write(str(xz)+' '+str(yz))
+
+
+
+
+
+    sky_center = (420, 145)  # (y, x)
+    inner_radius = 24  # 内半径
+    outer_radius = 29  # 外半径
+
+    # 中抜き円状のsky領域を作成し、その平均カウント数を計算
+    mask = create_annulus_mask(data.shape, sky_center, inner_radius, outer_radius)
+    sky_region = data[mask]
+    sky_threshold = np.max(sky_region)
+
+    
+    x_start, x_end = 370, 470
+    y_start, y_end = 95, 195
+
+    region = data[y_start:y_end, x_start:x_end]
+
+    binary_region = (region > sky_threshold).astype(int)
+
+    # OpenCVを使用して星を検出し、その中心を求める
+    binary_image = (binary_region * 255).astype(np.uint8)  # OpenCVが扱いやすいように変換
+
+    contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+
+    # 星の中心を求める
+
+    
+    for contour in contours:
+        if cv2.contourArea(contour) < 10:
+            continue
+        (x, y), radius = cv2.minEnclosingCircle(contour)
+        xz = x + 370
+        yz = y + 95
+        with open(compa_path, mode='w') as f:
+            f.write(str(xz)+' '+str(yz))
+    
+        
+        
+    
+ #--------------------------------------------------------------
+    hdu.close()
 
 data = {
     'FLUX': FLUX,
@@ -131,16 +249,19 @@ data = {
 }
 df = pd.DataFrame(data)
 
-# データフレームをテキストファイルに保存
-df.to_csv(f"/Users/takuto/iriki/{object}/{object}_data.txt", index = False, sep=',')
 
-# plt.scatter(range(len(FLUX)),FLUX)
-# plt.scatter(TIME,MAG)
-# plt.xlabel('JD')
-# plt.ylabel('Flux')
-# plt.title('qFlux')
-# plt.grid(True)
-# plt.show()
+dt_now = datetime.datetime.now()
+# データフレームをテキストファイルに保存
+df.to_csv(f"/Users/takuto/iriki/{object}/{object}{dt_now}_data.txt", index = False, sep=',')
+
+plt.scatter(range(len(FLUX)),FLUX) 
+# plt.scatter(TIME,FLUX)
+plt.xlabel('JD')
+plt.ylabel('Flux')
+plt.title('qFlux')
+plt.grid(True)
+plt.ylim(0.9,1.0)
+plt.savefig(f"/Users/takuto/iriki/{object}/{object}{dt_now}.png")
 
 
 
